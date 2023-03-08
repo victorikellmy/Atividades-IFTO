0258e5b722e2bb2f8b5ae6f995411c06970cfa69
victorikellmy <victorikellmyferreira123@gmail.com>
2023-03-08 15:34:12 -0300

    nova versao

--- Simulaçao_Emprestimos/Iniciador.java
+++ Simulaçao_Emprestimos/Iniciador.java
@@ -3,10 +3,10 @@ package Simulaçao_Emprestimos;
 public class Iniciador {
     public static void main(String[] args) {
         Cliente cliente = new Cliente("pedro", 10000.00);
-        Emprestimo emprestimo = new Emprestimo(cliente, 1000, 10, 5);
+        Emprestimo emprestimo = new Emprestimo(cliente, 1000, 10, 10);
 
         emprestimo.aprovaçao();
-        emprestimo.valorParcelas();
-        System.out.println("valor total pago:" + emprestimo.valorTotal());
+        System.out.printf("valor total pago: %.2f %n", (emprestimo.valorTotal()));
+        System.out.printf("valor das parcelas: %.2f %n", (emprestimo.valorParcelas()));
     }
 }
